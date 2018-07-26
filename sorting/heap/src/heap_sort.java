7cfbc45d59b4a8906b942239a6f4affb42d0eda3
lkys221 <lkys221@snu.ac.kr>
2018-07-26 14:51:25 +0900

    heap sort

--- sorting/heap/src/heap_sort.java
+++ sorting/heap/src/heap_sort.java
@@ -2,6 +2,18 @@ public class heap_sort {
 
     public static void main(String[] args) {
 
+        int[] array = {5, 11, 8, 14, 1, 3, 9, 15, 16, 2, 6, 12};
 
+
+    }
+
+    private static void heapSort(int[] arr){
+
+    }
+
+    private static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
