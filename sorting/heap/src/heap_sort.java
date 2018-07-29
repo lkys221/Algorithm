d2fd9b6138b72631c3a15be022e8ca65a1590cec
lkys221 <lkys221@snu.ac.kr>
2018-07-29 18:04:59 +0900

    heapfiy

--- sorting/heap/src/heap_sort.java
+++ sorting/heap/src/heap_sort.java
@@ -2,8 +2,9 @@ public class heap_sort {
 
     public static void main(String[] args) {
 
-        int[] array = {5, 11, 8, 14, 1, 3, 9, 15, 16, 2, 6, 12};
-        heapSort(array);
+        int[] array = {-1, 5, 8, 14, 1, 3, 9, 15, 16, 2, 6, 12};
+        heapfiy(array, 1);
+        print(array);
 
 
     }
@@ -17,6 +18,26 @@ public class heap_sort {
     }
 
     private static void heapfiy(int[] arr, int num){
+        int k, tmp;
+        if(num*2 > arr.length)
+            return;
+        else{
+            if(arr[num*2] > arr[num*2 +1])
+                k = num*2;
+            else
+                k = num*2 +1;
+
+            if(arr[num] >=  arr[k])
+                return;
+            else{
+                tmp = arr[num];
+                arr[num] = arr[k];
+                arr[k] = tmp;
+                print(arr);
+                heapfiy(arr, k);
+
+            }
+        }
 
     }
 
@@ -24,5 +45,6 @@ public class heap_sort {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 }
